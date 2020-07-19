2d1b83b2ad3203f05b13b93a96a434d8706e4c39
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-19 22:47:19 +0200

    new sections

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -5,9 +5,28 @@ import { Link } from "react-router-dom";
 const Home = () => {
   const types = [
     {
-      name: "sorting",
+      route: "sorting",
+      name: "Sorting",
       desc:
         "In computer science, a sorting algorithm is an algorithm that puts elements of a list in a certain order. Efficient sorting is important for optimizing the efficiency of other algorithms (such as search and merge algorithms) that require input data to be in sorted lists.",
+    },
+    {
+      route: "pathfinding",
+      name: "Pathfinding",
+      desc:
+        "Pathfinding or pathing is the plotting, by a computer application, of the shortest route between two points. It is a more practical variant on solving mazes. At its core, a pathfinding method searches a graph by starting at one vertex and exploring adjacent nodes until the destination node is reached, generally with the intent of finding the cheapest route.",
+    },
+    {
+      route: "automata",
+      name: "Cellular Automata",
+      desc:
+        "A cellular automaton consists of a regular grid of cells, each in one of a finite number of states, such as on and off. The grid can be in any finite number of dimensions. For each cell, a set of cells called its neighborhood is defined relative to the specified cell. An initial state (time t = 0) is selected by assigning a state for each cell. A new generation is created (advancing t by 1), according to some fixed rule (generally, a mathematical function) that determines the new state of each cell in terms of the current state of the cell and the states of the cells in its neighborhood. Typically, the rule for updating the state of cells is the same for each cell and does not change over time, and is applied to the whole grid simultaneously.",
+    },
+    {
+      route: "genetic",
+      name: "Genetic",
+      desc:
+        "A genetic algorithm (GA) is a metaheuristic inspired by the process of natural selection that belongs to the larger class of evolutionary algorithms (EA). Genetic algorithms are commonly used to generate high-quality solutions to optimization and search problems by relying on biologically inspired operators such as mutation, crossover and selection.",
     },
   ];
 
@@ -24,9 +43,14 @@ const Home = () => {
       </a>
       <hr />
       {types.map((t, i) => (
-        <Link key={i} style={{ textDecoration: "none" }} to={"/" + t.name}>
-          <Button className="mt-4" block variant="light">
-            <h4> {t.name[0].toUpperCase() + t.name.slice(1)}</h4>
+        <Link key={i} style={{ textDecoration: "none" }} to={"/" + t.route}>
+          <Button
+            disabled={i !== 0 ? true : false}
+            className="mt-4"
+            block
+            variant="light"
+          >
+            <h4>{t.name}</h4>
             {t.desc}
           </Button>
         </Link>

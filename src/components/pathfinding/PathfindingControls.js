181b359a833877225360b1caa7064c8d21e6c473
Alberto Mosconi <albertomaria.mosconi@gmail.com>
2020-07-23 22:53:19 +0200

    pathfinding controls

--- src/components/pathfinding/PathfindingControls.js
+++ src/components/pathfinding/PathfindingControls.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ButtonGroup, Dropdown, DropdownButton } from "react-bootstrap";
+import { PathfindingContext } from "./PathfindingContext";
 
 const PathfindingControls = () => {
-  return <div></div>;
+  const { algs, currentAlg, setCurrentAlg } = useContext(PathfindingContext);
+  return (
+    <div
+      style={{
+        position: "absolute",
+        bottom: 0,
+        minWidth: "100%",
+        display: "flex",
+        justifyContent: "center",
+        paddingBottom: 10,
+      }}
+    >
+      <DropdownButton
+        as={ButtonGroup}
+        id={"dropdown-button-drop-up"}
+        drop={"up"}
+        variant="secondary"
+        title={"Algorithm"}
+      >
+        {algs.map((alg, i) => (
+          <Dropdown.Item
+            onClick={(e) => {
+              setCurrentAlg(i);
+            }}
+            key={i}
+            className={i === currentAlg ? "bg-primary text-light" : ""}
+          >
+            <strong>{alg.name}</strong>
+          </Dropdown.Item>
+        ))}
+      </DropdownButton>
+    </div>
+  );
 };
 
 export default PathfindingControls;
